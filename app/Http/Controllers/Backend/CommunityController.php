054b155e32c10e5015929e8e8f8d4be8386e99ab
Ömer Ulusoy <uulsoy.omer09@gmail.com>
2022-10-02 21:20:53 +0300

    create community

--- app/Http/Controllers/Backend/CommunityController.php
+++ app/Http/Controllers/Backend/CommunityController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CommunityStoreRequest;
+use App\Models\Community;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CommunityController extends Controller
 {
@@ -14,28 +17,29 @@ class CommunityController extends Controller
      */
     public function index()
     {
-        //
+        return 'ok';
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Inertia\Response
      */
     public function create()
     {
-        //
+        return Inertia::render('Communities/Create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request)
+    public function store(CommunityStoreRequest $request)
     {
-        //
+        Community::create($request->validated() + ['user_id' => auth()->id()]);
+
+        return to_route('communities.index');
     }
 
     /**
---
+++ resources/js/Pages/Communities/Create.vue
@@ -0,0 +1,67 @@
+<template>
+    <Head title="Create Community"/>
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Create Community
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="max-w-md mx-auto bg-white m-2 p-6">
+                    <form @submit.prevent="submit">
+                        <div>
+                            <InputLabel for="name" value="Name"/>
+                            <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" autofocus
+                                       autocomplete="name"/>
+                            <InputError class="mt-2" :message="form.errors.name"/>
+                        </div>
+
+                        <div class="mt-1">
+                            <InputLabel for="slug" value="Slug"/>
+                            <TextInput id="slug" type="text" class="mt-1 block w-full" v-model="form.slug"
+                                        autocomplete="slug"/>
+                            <InputError class="mt-2" :message="form.errors.slug"/>
+                        </div>
+
+                        <div class="mt-1">
+                            <InputLabel for="description" value="Description"/>
+                            <TextInput id="description" type="text" class="mt- block w-full" v-model="form.description"
+                                        autocomplete="description"/>
+                            <InputError class="mt-2" :message="form.errors.description"/>
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }"
+                                           :disabled="form.processing">
+                                Store
+                            </PrimaryButton>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import {Head, useForm} from '@inertiajs/inertia-vue3';
+
+const form = useForm({
+    name: '',
+    description: '',
+    slug: '',
+});
+
+const submit = () => {
+    form.post(route('communities.store'));
+};
+</script>
+
